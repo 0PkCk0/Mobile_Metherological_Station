e782234e5bad7c27e9c37174ab3c72f7fe291fbd
FrancescoPietri <129965461+FrancescoPietri@users.noreply.github.com>
2023-11-20 15:56:49 +0100

    Update bmp280.h

--- Libraries/BMP280/bmp280.h
+++ Libraries/BMP280/bmp280.h
@@ -1,63 +1,73 @@
-/* bmp280.h
- * Written by Connor McKee and Michael Georgariou
- * CPE 316 - Spring 2020
- *
- * For use with the MSP432. */
-#include "msp.h"
+/*BMP280 barometric pressure and temperature sensor C Driver*/
+/*Reza Ebrahimi - https://github.com/ebrezadev */
+/*Version 1.0*/
 
-#ifndef BMP280_H_
-#define BMP280_H_
+#include "bmp280_defs.h"
 
-/* SPI pin definitions */
-#define CS_PORT P3
-#define CS_BIT  BIT6
+#ifndef __BMP280_H__
+#define __BMP280_H__
 
-#define SPI_PORT P1
-#define CLK_BIT BIT5
-#define MOSI_BIT BIT6
-#define MISO_BIT BIT7
+typedef enum {BMP280_TRUE, BMP280_FALSE} output_status_t;
 
-/* BMP Address definitions */
-#define CTRL_ADDR 0xF4
-#define PRESS_MSB_ADDR 0xF7
-#define PRESS_LSB_ADDR 0xF8
-#define PRESS_XLSB_ADDR 0xF9
-#define TEMP_MSB_ADDR 0xFA
-#define TEMP_LSB_ADDR 0xFB
-#define TEMP_XLSB_ADDR 0xFC
-#define CALIB_START_ADDR 0x88
-#define CALIB_ADDR_WIDTH 24
-#define CALIB_DATA_WIDTH 12
+typedef enum {OVERSAMPLING_0X = 0X00, OVERSAMPLING_1X = 0X01, OVERSAMPLING_2X = 0X02, OVERSAMPLING_4X = 0X03, OVERSAMPLING_8X = 0X04, OVERSAMPLING_16X = 0X05, OVERSAMPLING_ERROR = 0X06} over_sampling_t;
 
-/* BMP calibration table indices */
-#define DIG_T1 0
-#define DIG_T2 1
-#define DIG_T3 2
-#define DIG_P1 3
-#define DIG_P2 4
-#define DIG_P3 5
-#define DIG_P4 6
-#define DIG_P5 7
-#define DIG_P6 8
-#define DIG_P7 9
-#define DIG_P8 10
-#define DIG_P9 11
+typedef enum {I2C_ADDRESS_1 = 0X76, I2C_ADDRESS_2 = 0X77, BOTH_DETECTED, NONE_DETECTED} i2c_address_t;
 
+typedef enum {T_STANDBY_500US = 0X00, T_STANDBY_62500US, T_STANDBY_125MS, T_STANDBY_250MS, T_STANDBY_500MS, T_STANDBY_1S, T_STANDBY_2S, T_STANDBY_4S, T_STANDBY_ERROR} standby_time_t;
 
-/* BMP modes */
-#define ULTRA_LOW_RES 0x27
-#define LOW_RES 0x4B
+typedef enum {FILTER_OFF = 0X00, FILTER_2X, FILTER_4X, FILTER_8X, FILTER_16X, FILTER_ERROR} iir_filter_t;
 
-#define PRESSURE_READ 1
-#define TEMPERATURE_READ 0
+typedef enum {MODE_SLEEP = 0X00, MODE_FORCED = 0X01, MODE_NORMAL = 0X03, MODE_ERROR = 0X04} operation_mode_t;
 
-/* Function Prototypes */
-int32_t read_from_BMP(uint8_t mode);
-void SPI_init();
-void BMP_init();
-uint32_t compensate_pressure(int32_t uncomp_press);
-int32_t compensate_temp(int32_t uncomp_temp);
+typedef struct 
+{
+  uint16_t T1; 
+  int16_t T2;  
+  int16_t T3;  
+  uint16_t P1; 
+  int16_t P2; 
+  int16_t P3;  
+  int16_t P4;  
+  int16_t P5;  
+  int16_t P6;  
+  int16_t P7; 
+  int16_t P8;  
+  int16_t P9; 
+} calibration_param_t;
 
+typedef struct
+{
+  float temperature;
+  uint32_t pressure;
+  float altitude;
+} sensors_t;
 
+i2c_address_t bmp280_init();
+output_status_t bmp280_check_id(uint8_t deviceAddress);
+i2c_address_t bmp280_check_connected_address();
+void bmp280_reset(uint8_t deviceAddress);
+void bmp280_set_mode(uint8_t deviceAddress, operation_mode_t operationMode);
+void bmp280_set_pressure_accuracy(uint8_t deviceAddress, over_sampling_t osValue);
+void bmp280_set_temperature_accuracy(uint8_t deviceAddress, over_sampling_t osValue);
+void bmp280_set_standby_time(uint8_t deviceAddress, standby_time_t standbyTime);
+void bmp280_set_filter_coefficient(uint8_t deviceAddress, iir_filter_t filterCoefficient);
+void bmp280_set(uint8_t deviceAddress, operation_mode_t operationMode, over_sampling_t tempOS, over_sampling_t pressureOS, standby_time_t standbyTime, iir_filter_t filterCoefficient);
+void bmp280_set_default(uint8_t deviceAddress);
+int32_t bmp280_get_temperature(uint8_t deviceAddress);
+uint32_t bmp280_get_pressure(uint8_t deviceAddress);
+float bmp280_calculate_altitude_quick(uint8_t deviceAddress, uint32_t barometricPressure);
+float bmp280_calculate_altitude_hypsometric(uint8_t deviceAddress, uint32_t barometricPressure, float ambientTemperatureInC);
+sensors_t bmp280_get_all(uint8_t deviceAddress);
+operation_mode_t bmp280_get_mode(uint8_t deviceAddress);
+over_sampling_t bmp280_get_temperature_oversampling(uint8_t deviceAddress);
+over_sampling_t bmp280_get_pressure_oversampling(uint8_t deviceAddress);
+standby_time_t bmp280_get_standby_time(uint8_t deviceAddress);
+iir_filter_t bmp280_get_filter_coefficient(uint8_t deviceAddress);
 
-#endif /* BMP280_H_ */
+void bmp280_i2c_init();
+void bmp280_read_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
+void bmp280_write_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
+void delay_function(uint32_t delayMS);
+float power_function (float x, float y);
+
+#endif
